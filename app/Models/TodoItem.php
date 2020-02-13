cfc9b9a178829b2379df0b810f10c7af77b37ef6
mariu <mt@timbed.com>
2020-02-13 21:26:37 +0100

    Project Setup, Created initial models.

---
+++ app/Models/TodoItem.php
@@ -0,0 +1,23 @@
+<?php
+
+namespace App;
+
+use Illuminate\Database\Eloquent\Model;
+use Illuminate\Database\Eloquent\SoftDeletes;
+
+class TodoItem extends Model
+{
+    const STATUS_DONE = "done";
+    const STATUS_STARTED = "started";
+    const STATUS_SCHEDULED = "schedules";
+    const STATUS_NEW = "new";
+
+    //
+    use SoftDeletes;
+
+    protected $attributes = [
+        'status' => self::STATUS_NEW,
+        'reminder' => true,
+    ];
+
+}
---
+++ database/migrations/2020_02_13_193005_create_todo_items_table.php
@@ -0,0 +1,38 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+use phpDocumentor\Reflection\Types\Nullable;
+
+class CreateTodoItemsTable extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('todo_items', function (Blueprint $table) {
+            $table->bigIncrements('id');
+            $table->string('title');
+            $table->string('description');
+            $table->string('status');
+            $table->time('estimate');
+            $table->dateTime('deadline');
+            $table->boolean('reminder');
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('todo_items');
+    }
+}
